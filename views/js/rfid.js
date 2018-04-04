33a51fdaa2779dd587dc118c2b03030ce9ceb1bc
xdv <xdv@freeside.atm.loc>
2018-04-04 17:05:49 +0600

    работа с диспенсером

--- views/js/rfid.js
+++ views/js/rfid.js
@@ -6,15 +6,16 @@ const RFID_URL = 'http://localhost:8012/index.php';
 // получение содержимого экрана с сервера
 function getCard(params) {
     'use strict';
-
+    
+    var action = params.action || 'move',
+    screen = params.screen || '1',
+    timeout = params.timeout || '120';
+    
     var req = {
         action: 'read',
-        timeout: params.timeout
+        timeout: timeout
     };
-
-    var action = params.action || 'move',
-        screen = params.screen || '1';
-
+    
     // $('#loadingMessage').show();
     $.post(RFID_URL, req, function (response) {
         if (response.code === 0) {
@@ -23,9 +24,9 @@ function getCard(params) {
             doAction('move', 1, {});
         }
     }, 'json')
-        .fail(function () {
-            // скрываем сообщение "подождите"
-            doAction('move', 1, {});
-            stopAjax = 0;
-        });
+    .fail(function () {
+        // скрываем сообщение "подождите"
+        doAction('move', 1, {});
+        stopAjax = 0;
+    });
 }
